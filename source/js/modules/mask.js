9975fcda27e12bc26ec7bcce4d6b883223e38322
rmoongit <emil_tt@mail.ru>
2022-11-05 21:40:51 +0300

    Исправил mask.js

--- source/js/modules/mask.js
+++ source/js/modules/mask.js
@@ -1,9 +1,10 @@
-const phoneElement = document.querySelectorAll('[data-form="phone"]');
+import createMask from 'imask';
 
+const phoneElement = document.querySelectorAll('[data-form="phone"]');
 const initMask = () => {
 
   phoneElement.forEach((element) => {
-    window.IMask(element, {
+    createMask(element, {
       mask: '+{7} (000) 000 00 00',
       lazy: true, // make placeholder always visible
       placeholderChar: ' ', // defaults to '_'
--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,5 +1,3 @@
 // Swiper 7.4.1
 // import './vendor/swiper';
 import './vendor/focus-visible-polyfill';
-import IMask from 'imask';
-window.IMask = IMask;
